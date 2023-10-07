f800e51536f5b0aa6fe3abdb3cfc75942c956a71
Gmanka <gmankab@gmail.com>
2023-10-08 02:49:55 +0400

    fixed typo

--- plugins/core.lua
+++ plugins/core.lua
@@ -5,7 +5,7 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        "жмышенко валерий албертович",
+        "жмышенко валерий альбертович",
       }
       return opts
     end,
